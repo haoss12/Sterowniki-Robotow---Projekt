d2ecbc2b94f8cc5c75f0268643446753b15af8aa
CrassusXY <75444051+CrassusXY@users.noreply.github.com>
2023-05-29 12:43:05 +0200

    added accelerometer calibration

--- code/Core/Inc/imu.h
+++ code/Core/Inc/imu.h
@@ -6,11 +6,38 @@
 #include "../../../Drivers/BSP/STM32L476G-Discovery/stm32l476g_discovery_compass.h"
 #include <math.h>
 
-#define ACC_SENSITIVITY  0.0061
+#define ACC_SENSITIVITY  0.00061
 #define GYRO_SENSITIVITY 0.0175
-#define ALPHA 0.98		//filtration rate
+#define MAG_SENSITIVITY 0.058
+#define ALPHA 0.95		//filtration rate
 #define DT 0.05			//time tick
 
-void IMU(float *angX, float *angY, float *angZ);
+#define MAGSI1 -30.416148;
+#define MAGSI2 26.435598;
+#define MAGSI3 8.019681;
+#define MAGHI11 1.039317
+#define MAGHI12 0.023616
+#define MAGHI13 -0.003230
+#define MAGHI21 0.023616
+#define MAGHI22 1.075811
+#define MAGHI23 -0.000486
+#define MAGHI31 -0.003230
+#define MAGHI32 -0.000486
+#define MAGHI33 1.124874
+
+#define ACCSI1 0.361457;
+#define ACCSI2 0.415663;
+#define ACCSI3 0.018694;
+#define ACCHI11 0.982255
+#define ACCHI12 -0.001020
+#define ACCHI13 -0.000266
+#define ACCHI21 -0.001020
+#define ACCHI22 0.989506
+#define ACCHI23 -0.001753
+#define ACCHI31 -0.000266
+#define ACCHI32 -0.001753
+#define ACCHI33 0.995164
+
+void IMU(float *gyroX, float *gyroY, float *gyroZ, float *accX, float *accY, float *accZ, float *magX, float *magY, float *magZ);
 
 #endif /* INC_IMU_H_ */
--- code/Core/Src/imu.c
+++ code/Core/Src/imu.c
@@ -1,28 +1,64 @@
 #include "imu.h"
 
-void IMU(float *angX, float *angY, float *angZ) {
-	int16_t accRaw[3] = {0, 0, 0};
+
+//Roll -> Y
+//Pitch -> X
+//Yaw -> Z
+
+
+void IMU(float *gyroX, float *gyroY, float *gyroZ, float *accX, float *accY, float *accZ, float *magX, float *magY, float *magZ)
+{
 	float gyroRaw[3]  = {0.0f, 0.0f, 0.0f};
+	int16_t accRaw[3] = {0, 0, 0};
+	float acc[3] = {0.0f, 0.0f, 0.0f};
+	int16_t magRaw[3] = {0, 0, 0};
+	float mag[3] = {0.0f, 0.0f, 0.0f};
 
 	BSP_GYRO_GetXYZ(gyroRaw);
 	BSP_COMPASS_AccGetXYZ(accRaw);
-	//raw to m/s^2
-	float accX = (float)accRaw[0] * ACC_SENSITIVITY;
-	float accY = (float)accRaw[1] * ACC_SENSITIVITY;
-	float accZ = (float)accRaw[2] * ACC_SENSITIVITY;
+	BSP_COMPASS_MagGetXYZ(magRaw);
 
+	//GYRO
 	//gyro values to degrees
-	float gyroX = gyroRaw[0] * GYRO_SENSITIVITY * DT;
-	float gyroY = gyroRaw[1] * GYRO_SENSITIVITY * DT;
-	float gyroZ = gyroRaw[2] * GYRO_SENSITIVITY * DT;
+	*gyroX = (float)gyroRaw[0] * GYRO_SENSITIVITY * DT;
+	*gyroY = (float)gyroRaw[1] * GYRO_SENSITIVITY * DT;
+	*gyroZ = (float)gyroRaw[2] * GYRO_SENSITIVITY * DT;
 
-	//acc values to degrees
-	float accAngX = (float)((atan2(accY, accZ)) + M_PI ) * (180 / M_PI);
-	float accAngY = (float)((atan2(accZ, accX)) + M_PI ) * (180 / M_PI);
+	//ACC
+	//raw data to m/s^2 with soft iron
+	acc[0] = (float)accRaw[0] * ACC_SENSITIVITY - ACCSI1;
+	acc[1] = (float)accRaw[1] * ACC_SENSITIVITY - ACCSI2;
+	acc[2] = (float)accRaw[2] * ACC_SENSITIVITY - ACCSI3;
+	//hard iron
+	*accX = ACCHI11 * acc[0] + ACCHI12 * acc[1] + ACCHI13 * acc[2];
+	*accY = ACCHI21 * acc[0] + ACCHI22 * acc[1] + ACCHI23 * acc[2];
+	*accZ = ACCHI31 * acc[0] + ACCHI32 * acc[1] + ACCHI33 * acc[2];
 
-	//complementary filtration and position calculation
-	*angX = ALPHA * (gyroX * DT + *angX) + (1.0f - ALPHA) * accAngX;
-	*angY = ALPHA * (gyroY * DT + *angY) + (1.0f - ALPHA) * accAngY;
-	//roll filtration and calculation
-	*angZ = (gyroZ * DT + *angZ);
+	//MAG
+	//Calculate raw data to nanotesla with soft iron
+	mag[0] = magRaw[0] * MAG_SENSITIVITY - MAGSI1;
+	mag[1] = magRaw[1] * MAG_SENSITIVITY - MAGSI2;
+	mag[2] = magRaw[2] * MAG_SENSITIVITY - MAGSI3;
+	//hard iron
+	*magX = MAGHI11 * mag[0] + MAGHI12 * mag[1] + MAGHI13 * mag[2];
+	*magY = MAGHI21 * mag[0] + MAGHI22 * mag[1] + MAGHI23 * mag[2];
+	*magZ = MAGHI31 * mag[0] + MAGHI32 * mag[1] + MAGHI33 * mag[2];
+
+
+//	//acc values to degrees
+//	float accAngX = (float)((atan2(accY, accZ)) + M_PI ) * (180 / M_PI);
+//	float accAngY = (float)((atan2(accZ, accX)) + M_PI ) * (180 / M_PI);
+//
+//	accAngY -= 270.0f;
+//    if (accAngY < -180.0f)
+//    	accAngY += 360.0f;
+//
+//    accAngX -= 180.0f;
+//
+//
+//	//complementary filtration and position calculation
+//	*angX = ALPHA * (gyroX * DT + *angX) + (1.0f - ALPHA) * accAngX;
+//	*angY = ALPHA * (gyroY * DT + *angY) + (1.0f - ALPHA) * accAngY;
+//	//roll filtration and calculation
+//	*angZ = (gyroZ * DT + *angZ);
 }
